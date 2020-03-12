fe4037e9a3daa10ffe52eb3370077678cee807aa
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-03-12 11:28:49 -0500

    Image sharp component

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
+import {Image} from './'
 
 export default function Jumbo() {
   return (
@@ -8,6 +9,7 @@ export default function Jumbo() {
         <h2>¡Consigue el mejor swag exclusivo de Platzi!</h2>
         <small>Piezas elegantes para los mejores estudiantes</small>
       </div>
+	  <Image name="icon" />
     </StyledJumbo>
   )
 }
